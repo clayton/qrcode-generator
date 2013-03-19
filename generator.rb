d8265524f3361fd764073ac3771b4818a02e65f9
Clayton Lengel-Zigich <clayton@claytonlz.com>
2013-03-19 16:47:43 -0700

    Use URI

--- generator.rb
+++ generator.rb
@@ -1,17 +1,17 @@
 require 'rqrcode_png'
 require 'chunky_png'
+require 'uri'
 
 class Generator
   def initialize(url)
     @url = url
+    @uri = URI.parse(url)
   end
 
   def generate
-    qr = RQRCode::QRCode.new( @url, :size => 4, :level => :h )
+    qr = RQRCode::QRCode.new( @url, :size => 10, :level => :h )
     png = qr.to_img
-    png.resize(400, 400).save("qr.png")
+    file_name = @uri.path.downcase.slice(1,@uri.path.length).gsub("/", "-")
+    png.resize(400, 400).save("#{file_name}.png")
   end
 end
-
-Generator.new("http://google.com").generate
-`open qr.png`
